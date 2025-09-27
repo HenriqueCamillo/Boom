184e2ab89790e4777a60fa042907727446b8939b
HenriqueCamillo <henriquematarazocamillo@gmail.com>
2025-09-27 17:00:32 -0300

    Skill Collectable and its organizer in scene

---
+++ Assets/Scripts/SkillCollectableOrganizer.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillCollectableOrganizer : MonoBehaviour
+{
+    [SerializeField] Transform spawnPosition;
+    [SerializeField] float distanceBetweenSpawns;
+
+    [SerializeField] List<SkillCollectable> skillCollectables;
+
+    private void Awake()
+    {
+        bool hasEvenNumberOfSkills = skillCollectables.Count % 2 == 0;
+        for (int i = 0; i < skillCollectables.Count; i++)
+        {
+            float xPosition = spawnPosition.transform.position.x;
+            int sign = (i % 2 == 0) ? 1 : -1;
+            if (hasEvenNumberOfSkills)
+                xPosition += ((distanceBetweenSpawns / 2) + (distanceBetweenSpawns * (i / 2))) * sign;
+            else
+                xPosition += (distanceBetweenSpawns * ((i + 1) / 2)) * sign;
+
+            skillCollectables[i].transform.position = new Vector3( + xPosition, spawnPosition.transform.position.y, spawnPosition.transform.position.z);
+        }
+    }
+}
