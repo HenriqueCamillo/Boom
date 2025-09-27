d2a337b6a57372aa02b9912cb0b1a07cfac4c740
HenriqueCamillo <henriquematarazocamillo@gmail.com>
2025-09-27 11:24:32 -0300

    Basic camera switching between hub and level (without triggers and interactables yet)

---
+++ Assets/Scripts/CameraSwitcher.cs
@@ -0,0 +1,41 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] Camera hubCamera;
+    [SerializeField] Camera levelCamera;
+
+    private Vector3 hubCameraPosition;
+    private Vector3 levelCameraPosition;
+
+    [Space(5)]
+
+    [Header("Transition")]
+    [SerializeField] float transitionDuration;
+    [SerializeField] Ease ease;
+
+    private void Awake()
+    {
+        hubCameraPosition = hubCamera.transform.position;
+        levelCameraPosition = levelCamera.transform.position;
+
+        Camera.main.transform.position = hubCameraPosition;
+    }
+
+    public void ViewHub()
+    {
+        TransitionTo(hubCameraPosition);
+    }
+
+    public void ViewLevel()
+    {
+        TransitionTo(levelCameraPosition);
+    }
+
+    private void TransitionTo(Vector3 position)
+    {
+        Camera.main.transform.DOMove(position, transitionDuration).SetEase(ease);
+    }
+}
