7942811da626727add3021a8a9ab9d3b3bbb1cd6
HenriqueCamillo <henriquematarazocamillo@gmail.com>
2025-09-28 11:19:23 -0300

    End condition

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,6 +5,6 @@ public class SceneLoader : MonoBehaviour
 {
     public void GoToNextScene()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings);
     }
 }
